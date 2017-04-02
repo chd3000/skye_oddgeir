c88694a99993e28e2ce2f9e2e1f61c5b58dda8c4
Chris <chris@abc.de>
2017-04-02 10:54:36 +0200

    add app db

--- skye_oddgeir/settings.py
+++ skye_oddgeir/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'db',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_countries',
+
 ]
 
 MIDDLEWARE_CLASSES = [
